72f3873b29ced736bfe43f3753bb136eb3a99720
kenzaturki <kturki8@gmail.com>
2019-09-06 11:01:08 +0200

    [Agenda] Create agenda  - Created Agenda component  - Updated css style

---
+++ src/Agenda.js
@@ -0,0 +1,100 @@
+import React, {Component} from 'react'
+import Calendar from 'react-calendar'
+import TimeField from 'react-simple-timefield'
+
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import * as Icons from "@fortawesome/free-solid-svg-icons"
+
+import cookie from 'react-cookies'
+
+import './App.css'
+
+class Agenda extends Component {
+  state = {
+    title:'',
+    date: new Date(),
+    time: '09:00',
+    appointments: cookie.load('appointments')|| [],
+  }
+  
+  handleTitleChange = (event) => {
+    this.setState({title: event.target.value});
+  }
+
+  handleDateChange = date => {
+    this.setState({ date });
+  }
+
+  handleTimeChange = time => {
+    this.setState({ time });
+  }
+
+  addAppointment = (event) => {
+    const {title, date, time, appointments} = this.state;
+    var newAppointments = appointments;
+    var sDate= date.toDateString();
+    sDate = sDate.substring([0,15]);
+    newAppointments.push([title, sDate + ', ' + time]);
+    this.setState({title:'', date: new Date(), time: '09:00', appointments: newAppointments});
+    cookie.save('appointments', newAppointments);
+  }
+ 
+  onTrashClick = index => {
+    const appointments = this.state.appointments;
+    var newAppointments = appointments;
+
+    newAppointments.splice(index,1);
+    this.setState({appointment: newAppointments});
+    cookie.save('appointments', newAppointments);
+
+  }
+
+  render() {
+    const {title, date, time, appointments} = this.state;
+    return (
+      <div className="agenda">
+        <p className="title">
+          Agenda
+        </p>
+        <p>
+            <input 
+              type = "text"
+              placeholder="Make a new appoitment"
+              className="input"
+              maxLength={30}
+              onChange={this.handleTitleChange}  
+              value={title}
+            />
+        </p>
+      
+        <Calendar
+          className="calendar"
+          onChange={this.handleDateChange}
+          value={date}
+        />
+
+        <TimeField value={time} onChange={this.handleTimeChange} />
+        <p>
+          <button type="button" className="button" onClick={this.addAppointment}>Make Appointment</button>
+        </p>
+        <div className="appointment">
+          {appointments.map((appointment, index) => (
+            <div key={index}>
+              <p className="appTitle">
+                {appointment[0]}
+              </p>
+              <p className="appDate">
+                {appointment[1]}
+              </p>
+              <FontAwesomeIcon icon={Icons.faTrashAlt} className="delete" color="	#778899" onClick={()=>this.onTrashClick(index)}/>
+              <hr/>
+            </div>
+          ))}
+        </div>
+      </div>
+
+    );
+  }
+}
+
+export default Agenda;
--- src/App.js
+++ src/App.js
@@ -1,100 +1,15 @@
 import React, {Component} from 'react'
-import Calendar from 'react-calendar'
-import TimeField from 'react-simple-timefield'
 
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import * as Icons from "@fortawesome/free-solid-svg-icons"
+import Agenda from './Agenda';
 
-import cookie from 'react-cookies'
-
-import './App.css'
+import './App.css';
 
 class App extends Component {
-  state = {
-    title:'',
-    date: new Date(),
-    time: '09:00',
-    appointments: cookie.load('appointments')|| [],
-  }
-  
-  handleTitleChange = (event) => {
-    this.setState({title: event.target.value});
-  }
-
-  handleDateChange = date => {
-    this.setState({ date })
-  }
-
-  handleTimeChange = time => {
-    this.setState({ time })
-  }
-
-  addAppointment = (event) => {
-    event.preventDefault()
-    const {title, date, time, appointments} = this.state
-    var newAppointments = appointments
-    var sDate= date.toDateString()
-    sDate = sDate.substring([0,15])
-    newAppointments.push([title, sDate + ', ' + time])
-    this.setState({title:'', date: new Date(), time: '09:00', appointments: newAppointments})
-    cookie.save('appointments', newAppointments)
-  }
- 
-  onTrashClick = lom => {
-    const appointments = this.state.appointments
-    var newAppointments = appointments
-
-    newAppointments.splice(lom,1)
-    this.setState({appointment: newAppointments})
-    cookie.save('appointments', newAppointments)
-
-  }
 
   render() {
-    const {title, date, time, appointments} = this.state
     return (
       <div className="app">
-        <p className="make">
-          Agenda
-        </p>
-        <form onSubmit={this.addAppointment}>
-          <p>
-              <input 
-                type = "text"
-                placeholder="Make a new appoitment"
-                className="title"
-                maxLength={30}
-                onChange={this.handleTitleChange}  
-                value={title}
-              />
-          </p>
-        
-          <Calendar
-            className="calendar"
-            onChange={this.handleDateChange}
-            value={date}
-          />
-
-          <TimeField value={time} onChange={this.handleTimeChange} />
-          <p>
-            <button type="submit" className="button">Make Appointment</button>
-          </p>
-          <div className="appointment">
-            {appointments.map((appointment, index) => (
-              <div key={index}>
-                <p className="appTitle">
-                  {appointment[0]}
-                </p>
-                <p className="appDate">
-                  {appointment[1]}
-                </p>
-                <FontAwesomeIcon icon={Icons.faTrashAlt} className="delete" color="	#778899" onClick={()=>this.onTrashClick(index)}/>
-                <hr 
-                />
-              </div>
-            ))}
-          </div>
-        </form>
+        <Agenda/>
       </div>
 
     );
